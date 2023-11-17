aa1cd682c895b13a9c3658412a960855e41dec28
Miido04 <alerr0406@gmail.com>
2023-11-17 11:43:07 -0500

    Login

---
+++ app/src/main/java/com/example/proyecto/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.example.proyecto
+
+import android.app.Activity
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+open class BaseActivity : AppCompatActivity() {
+    private lateinit var pb: Dialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.progress_bar)
+    }
+
+    fun showProgressBar() {
+        pb = Dialog(this)
+        pb.setContentView(R.layout.progress_bar)
+        pb.setCancelable(false)
+        pb.show()
+    }
+
+    fun hideProgressBar() {
+        pb.dismiss()
+    }
+
+    fun showToast(activity: Activity, msg: String) {
+        Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+    }
+}
